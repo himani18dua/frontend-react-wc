2cd3c023008019dca42c5c3767558b991dc1759f
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-12 00:30:43 +0530

    Update CrawlLinks.js

--- src/CrawlLinks.js
+++ src/CrawlLinks.js
@@ -30,7 +30,7 @@ function CrawlLinks() {
 
     const fetchData = async () => {
         try {
-            const response = await fetch("https://flask-sample-ff32.onrender.com");
+            const response = await fetch("https://fin-back-odw1.onrender.com/members");
             if (!response.ok) {
                 throw new Error('Failed to fetch data');
             }

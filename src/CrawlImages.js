54c5d2876395efcb4b762289e3723e49e4924d0e
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-11 20:54:53 +0530

    Update CrawlImages.js

--- src/CrawlImages.js
+++ src/CrawlImages.js
@@ -1,15 +1,14 @@
 import React, { useState, useContext } from 'react';
 import DataContext from './DataContext';
-// import API_URL from './config';
 
-function CrawlLinks() {
+function CrawlImages() {
     const { data, setData } = useContext(DataContext);
     const [url, setUrl] = useState('');
 
-    const handleCrawl = async () => {
-        setData(prevData => ({ ...prevData, loadingLinks: true, errorLinks: null }));
+    const handleImageCrawl = async () => {
+        setData(prevData => ({ ...prevData, loadingImages: true, errorImages: null }));
         try {
-            const response = await fetch('https://fin-back-odw1.onrender.com/crawl', {
+            const response = await fetch('http://localhost:5000/img-crawl', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -18,76 +17,73 @@ function CrawlLinks() {
             });
 
             if (!response.ok) {
-                throw new Error('Crawl request failed');
+                throw new Error('Image Crawl request failed');
             }
 
             await new Promise(resolve => setTimeout(resolve, 2000));
-            fetchData();
+            fetchImageData();
         } catch (err) {
-            setData(prevData => ({ ...prevData, errorLinks: err.message, crawlLinksData: [], loadingLinks: false }));
+            setData(prevData => ({ ...prevData, errorImages: err.message, crawlImagesData: [], loadingImages: false }));
         }
     };
 
-    const fetchData = async () => {
+    const fetchImageData = async () => {
         try {
-            const response = await fetch("https://fin-back-odw1.onrender.com/members");
+            const response = await fetch("http://localhost:5000/img-members");
             if (!response.ok) {
-                throw new Error('Failed to fetch data');
+                throw new Error('Failed to fetch image data');
             }
             const data = await response.json();
-            setData(prevData => ({ ...prevData, crawlLinksData: data, loadingLinks: false }));
+            setData(prevData => ({ ...prevData, crawlImagesData: data, loadingImages: false }));
         } catch (error) {
-            console.error('Error fetching data:', error);
-            setData(prevData => ({ ...prevData, errorLinks: 'Failed to fetch data', loadingLinks: false }));
+            console.error('Error fetching image data:', error);
+            setData(prevData => ({ ...prevData, errorImages: 'Failed to fetch image data', loadingImages: false }));
         }
     };
 
-    const handleDownload = () => {
-        fetch('https://fin-back-odw1.onrender.com/download')
+    const handleImageDownload = () => {
+        fetch('http://localhost:5000/img-download')
             .then(response => response.blob())
             .then(blob => {
                 const url = window.URL.createObjectURL(new Blob([blob]));
                 const link = document.createElement('a');
                 link.href = url;
-                link.setAttribute('download', 'broken_links.pdf');
+                link.setAttribute('download', 'images_without_alt.pdf');
                 document.body.appendChild(link);
                 link.click();
                 link.parentNode.removeChild(link);
             })
-            .catch(err => console.error('Download failed:', err));
+            .catch(err => console.error('Image Download failed:', err));
     };
 
     return (
         <div>
-            <h1>Crawl Links</h1>
+            <h1>Crawl Images</h1>
             <input
                 type="text"
                 value={url}
                 onChange={(e) => setUrl(e.target.value)}
                 placeholder="Enter URL"
             />
-            <button onClick={handleCrawl}>Crawl</button>
-            {data.crawlLinksData.length > 0 && (
+            <button onClick={handleImageCrawl}>Crawl</button>
+            {data.crawlImagesData.length > 0 && (
                 <div>
-                    <button onClick={handleDownload}>Download PDF</button>
+                    <button onClick={handleImageDownload}>Download PDF</button>
                     <ul>
-                        <div className="container2"><p className="broken">Number of broken links found: {data.crawlLinksData.length}</p></div>
-                        {data.crawlLinksData.map((item, index) => (
+                    <div className="container2"><p className="broken">Number of images without alt text found: {data.crawlImagesData.length}</p></div>
+                        {data.crawlImagesData.map((item, index) => (
                             <li key={index}>
-                                <p><strong>Source Page:</strong> <a href={item.Source_Page} target="_blank" rel="noopener noreferrer">{item.Source_Page}</a></p>
-                                <p><strong>Link Text:</strong> {item.Link_Text}</p>
-                                <p><strong>Broken Page Link:</strong> <a href={item.Broken_Page_Link} target="_blank" rel="noopener noreferrer">{item.Broken_Page_Link}</a></p>
-                                <p><strong>HTTP Code:</strong> {item.HTTP_Code}</p>
-                                <p><strong>External:</strong> {item.External ? 'Yes' : 'No'}</p>
+                                <p><strong>Source Page:</strong> <a href={item.source_page} target="_blank" rel="noopener noreferrer">{item.source_page}</a></p>
+                                <p><strong>Image URL:</strong> <a href={item.image_url} target="_blank" rel="noopener noreferrer">{item.image_url}</a></p>
                             </li>
                         ))}
                     </ul>
                 </div>
             )}
-            {data.loadingLinks && <p>Loading...</p>}
-            {data.errorLinks && <p>Error: {data.errorLinks}</p>}
+            {data.loadingImages && <p>Loading...</p>}
+            {data.errorImages && <p>Error: {data.errorImages}</p>}
         </div>
     );
 }
 
-export default CrawlLinks;
+export default CrawlImages;
